2272756dda43c1573b88e76af42e08eba41d4e87
Samelafarias <124313487+Samelafarias@users.noreply.github.com>
2024-11-12 07:54:24 -0300

    teste de conexao

--- db_pj_chaves/index.js
+++ db_pj_chaves/index.js
@@ -40,7 +40,7 @@ function connectToDatabase() {
     db.connect((error) => {
         if (error) {
             console.error('Erro ao conectar com o banco de dados:', error);
-            setTimeout(connectToDatabase, 3000); // Tenta reconectar após 3 segundos
+            setTimeout(connectToDatabase, 2000); // Tenta reconectar após 2 segundos
         } else {
             console.log('Conectado ao banco de dados com sucesso!');
         }
